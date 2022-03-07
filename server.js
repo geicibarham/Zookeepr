2656309df10656867458917df66bb5ffed805db5
geicibarham <geicianecosta43@gmail.com>
2022-03-06 20:47:21 -0500

    lesson 1 done

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { animals } = require('./data/animals');
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 // app.get('/api/animals', (req, res) => {
@@ -34,25 +36,40 @@ function filterByQuery(query, animalsArray) {
       );
     });
   }
-  if(query.diet) {
+  if (query.diet) {
     filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
   }
-  if(query.species) {
+  if (query.species) {
     filteredResults = filteredResults.filter(animal => animal.species === query.species);
   }
-  if (query.name){
+  if (query.name) {
     filteredResults = filteredResults.filter(animal => animal.name === query.name);
   }
   return filteredResults;
 }
-app.get('/api/animals', (req, res) => {
-    let results = (animals);
-   if(req.query) {
-     results = filterByQuery(req.query , results);
-   }
-    res.json(results);
-  });
 
-  app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
+
+app.get('/api/animals', (req, res) => {
+  let results = (animals);
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if(result) {
+  res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}`);
+});
